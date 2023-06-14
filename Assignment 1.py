f4b1b4eb24077ede0c8fccb62d837c592ce5c645
Yousuf Ansari <136440089+devilsbreath10@users.noreply.github.com>
2023-06-14 21:49:47 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,8 @@
+# Average of three numbers
+marks1=int(input("Enter marks of subject 1: "))
+marks2=int(input("Enter marks of subject 2: "))
+marks3=int(input("Enter marks of subject 3: ")) 
+sum=marks1+marks2+marks3
+print("Sum of all subjects is",sum)
+avg=sum/3
+print("Average of 3 subjects is",avg)
